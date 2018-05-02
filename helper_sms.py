14de61b4018bc9f457c0cf8bceab449868db89f5
SmellyCat <smellycat2014@foxmail.com>
2018-05-02 12:05:09 +0800

    移除 basic auth

--- helper_sms.py
+++ helper_sms.py
@@ -1,29 +1,33 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 import json
 
 import requests
-from requests.auth import HTTPBasicAuth
 
 
 class SMS(object):
     def __init__(self, **kwargs):
         self.host = kwargs['host']
         self.port = kwargs['port']
-        self.user = kwargs['user']
-        self.pwd  = kwargs['pwd']
-        self.headers = {'content-type': 'application/json'}
+        self.headers = {
+            'content-type': 'application/json',
+            'apikey': kwargs['apikey']
+        }
+        self.path = '/sms'
+        self.status = False
 
-    def sms_send(self, content, mobiles):
+    def sms_send(self, content, mobiles, user):
         """发送短信"""
-        url = 'http://{host}:{port}/sms'.format(host=self.host, port=self.port)
-        data = {'content': content, 'mobiles': mobiles}
+        url = 'http://{0}:{1}{2}/sms'.format(self.host, self.port, self.path)
+        data = {'content': content, 'mobiles': mobiles, 'user_name': user}
         try:
-            r = requests.post(
-                url, headers=self.headers, data=json.dumps(data),
-		auth=HTTPBasicAuth(self.user, self.pwd))
+            r = requests.post(url, headers=self.headers, data=json.dumps(data))
             if r.status_code == 201:
                 return json.loads(r.text)
+            else:
+                self.status = False
+                raise Exception('url: {url}, status: {code}, {text}'.format(
+                    url=url, code=r.status_code, text=r.text))
         except Exception as e:
-            print 'sms:',e
+            self.status = False
             raise
 
